f487700d665550a6c20a3a163d9a6b8abe6c08e5
agi1004 <39743375+agi1004@users.noreply.github.com>
2024-04-09 13:10:50 +0900

    [Bronze I] Title: 더하기 사이클, Time: 132 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        FastReader fr = new FastReader(); // 문제 해결 소스 코드
+        int N = fr.nextInt();
+        int n = N;
+        int a, b, c;
+        int cycle = 0;
+        do {    
+            if (N < 10)
+                N = N * 10 + N;
+            else {
+                a = N / 10;
+                b = N % 10;
+                c = a + b;
+                N = b * 10 + c % 10;
+            }
+            cycle++;
+        } while (N != n);
+        System.out.println(cycle);
+    }
+    public static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader() { br = new BufferedReader(new InputStreamReader(System.in)); }
+        public FastReader(String s) throws FileNotFoundException { br = new BufferedReader(new FileReader(new File(s))); }
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try { st = new StringTokenizer(br.readLine()); }
+                catch (IOException e) { e.printStackTrace(); }
+            }
+            return st.nextToken();
+        }
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            String str = "";
+            try { str = br.readLine(); }
+            catch (IOException e) { e.printStackTrace(); }
+            return str;
+        }
+    }
+}
