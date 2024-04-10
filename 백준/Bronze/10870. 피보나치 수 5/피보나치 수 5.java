b60dba6276a5bc17ee60ed83d5c39fde0fddfbcd
agi1004 <39743375+agi1004@users.noreply.github.com>
2024-04-10 13:26:07 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 124 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+class Number {
+    public int n;
+    public Number(int n) { this.n = n; }
+    public int fibo(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fibo(n - 1) + fibo(n - 2);
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        FastReader fr = new FastReader(); // 문제 해결 소스 코드
+        int n = fr.nextInt();
+        Number num = new Number(n);
+        System.out.println(num.fibo(n));
+    }
+    public static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader() { br = new BufferedReader(new InputStreamReader(System.in)); }
+        public FastReader(String s) throws FileNotFoundException { br = new BufferedReader(new FileReader(new File(s))); }
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try { st = new StringTokenizer(br.readLine()); }
+                catch (IOException e) { e.printStackTrace(); }
+            }
+            return st.nextToken();
+        }
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            String str = "";
+            try { str = br.readLine(); }
+            catch (IOException e) { e.printStackTrace(); }
+            return str;
+        }
+    }
+}
