43ebb47bd7c61a9f685f68374a77886763b34a84
agi1004 <39743375+agi1004@users.noreply.github.com>
2024-04-10 13:25:18 +0900

    [Bronze III] Title: 최소, 최대, Time: 528 ms, Memory: 92080 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        FastReader fr = new FastReader(); // 문제 해결 소스 코드
+        int N = fr.nextInt();
+        int[] num = new int[N];
+        for (int i = 0; i < N; i++)
+            num[i] = fr.nextInt();
+        int min = 1000000, max = -1000000;
+        for (int i = 0; i < N; i++) {
+            if (num[i] < min)
+                min = num[i];
+            if (num[i] > max)
+                max = num[i];
+        }
+        System.out.print(min + " " + max);
+    }
+    public static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader() { br = new BufferedReader(new InputStreamReader(System.in)); }
+        public FastReader(String s) throws FileNotFoundException { br = new BufferedReader(new FileReader(new File(s))); }
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try { st = new StringTokenizer(br.readLine()); }
+                catch (IOException e) { e.printStackTrace(); }
+            }
+            return st.nextToken();
+        }
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            String str = "";
+            try { str = br.readLine(); }
+            catch (IOException e) { e.printStackTrace(); }
+            return str;
+        }
+    }
+}
