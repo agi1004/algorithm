aad7c1329ba717a77a3b9cd8d06accbe4d535037
agi1004 <39743375+agi1004@users.noreply.github.com>
2024-04-10 13:43:05 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 372 ms, Memory: 20900 KB -BaekjoonHub

---
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int count = 0;
+    public static int recursion(String s, int l, int r){
+        count++;
+        if(l >= r) return 1;
+        else if(s.charAt(l) != s.charAt(r)) return 0;
+        else return recursion(s, l+1, r-1);
+    }
+    public static int isPalindrome(String s){
+        return recursion(s, 0, s.length()-1);
+    }
+    public static void main(String[] args){
+        FastReader fr = new FastReader();
+        int T = fr.nextInt();
+        String[] S = new String[T];
+        for (int i = 0; i < T; i++)
+            S[i] = fr.next();
+        for (int i = 0; i < T; i++) {
+            System.out.println(isPalindrome(S[i]) + " " + count);
+            count = 0;
+        }
+    }
+    public static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader() { br = new BufferedReader(new InputStreamReader(System.in)); }
+        public FastReader(String s) throws FileNotFoundException { br = new BufferedReader(new FileReader(new File(s))); }
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try { st = new StringTokenizer(br.readLine()); }
+                catch (IOException e) { e.printStackTrace(); }
+            }
+            return st.nextToken();
+        }
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            String str = "";
+            try { str = br.readLine(); }
+            catch (IOException e) { e.printStackTrace(); }
+            return str;
+        }
+    }
+}
