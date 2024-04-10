ba1d4c28e3c3bb603ce17186cc4e15bc3f880f29
agi1004 <39743375+agi1004@users.noreply.github.com>
2024-04-10 13:42:32 +0900

    [Bronze IV] Title: 영수증, Time: 128 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        FastReader fr = new FastReader(); // 문제 해결 소스 코드
+        int X = fr.nextInt();
+        int N = fr.nextInt();
+        int a, b, sum = 0;
+        for (int i = 0; i < N; i++) {
+            a = fr.nextInt();
+            b = fr.nextInt();
+            sum += (a * b);
+        }
+        if (X == sum) System.out.println("Yes");
+        else System.out.println("No");
+    }
+    public static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader() { br = new BufferedReader(new InputStreamReader(System.in)); }
+        public FastReader(String s) throws FileNotFoundException { br = new BufferedReader(new FileReader(new File(s))); }
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try { st = new StringTokenizer(br.readLine()); }
+                catch (IOException e) { e.printStackTrace(); }
+            }
+            return st.nextToken();
+        }
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            String str = "";
+            try { str = br.readLine(); }
+            catch (IOException e) { e.printStackTrace(); }
+            return str;
+        }
+    }
+}
