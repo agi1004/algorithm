62d7880407aa00196d0ca9b3f4ef70e87b418e71
agi1004 <39743375+agi1004@users.noreply.github.com>
2024-04-17 05:14:41 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -1,11 +1,13 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 	static int MOD = 1000000000;
 	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
 		int[][] dp = new int[10][N + 1];
 		long count = 0;
 		
