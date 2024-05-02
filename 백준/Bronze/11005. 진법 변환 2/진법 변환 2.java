1698db90f85546ede838d214bcbdec3f50a8770e
agi1004 <39743375+agi1004@users.noreply.github.com>
2024-05-02 16:44:32 +0900

    [Bronze I] Title: 진법 변환 2, Time: 128 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -10,38 +10,9 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine()); 
-		int N = Integer.parseInt(st.nextToken());	// 10진법 수
-		int B = Integer.parseInt(st.nextToken());	// B진법
-		String result = "";
+		int N = Integer.parseInt(st.nextToken());	
+		int B = Integer.parseInt(st.nextToken());
 		
-//		N = 0;
-//		B = 8;
-//		N % B = 3;
-//		result = "053";
-//		
-//		N = 0;
-//		B = 16;
-//		N % B = 14;
-//		result = "8E";
-//		
-//		N = 0;
-//		B = 2;
-//		N % B = 1;
-//		result = "00010111";
-        
-		while (N != 0) {
-			if (B >= 11 && N % B >= 10){
-				result += (char)(N % B - 10 + 'A');
-			} else {
-				result += (char)(N % B + '0');
-			}
-			N /= B;
-		}
-		
-		for (int i = result.length() - 1; i >= 0; i--) {
-			bw.write(result.charAt(i));
-		}
-		
-		bw.flush();
-	}
+		System.out.println(Integer.toString(N, B).toUpperCase());	// 10진수 N -> B진수
+ 	}
 }
