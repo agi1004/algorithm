65628f4f07bc32ccb1af1cdde7b1c0ead3660822
agi1004 <39743375+agi1004@users.noreply.github.com>
2024-05-06 20:03:05 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 204 ms, Memory: 17696 KB -BaekjoonHub

--- 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -3,14 +3,8 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		long sum = 0;
-		
-		for (int i = 1; i < n; i++) {
-			sum += i;
-		}
-		
-		System.out.println(sum);
+		long n = sc.nextInt();
+		System.out.println(n * (n - 1) / 2);
 		System.out.println(2);
 	}
 }
