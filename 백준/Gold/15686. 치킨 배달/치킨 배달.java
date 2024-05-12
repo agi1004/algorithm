a599434f18a7c1b82cbce6212836d378372fec78
agi1004 <39743375+agi1004@users.noreply.github.com>
2024-05-13 00:45:23 +0900

    [Gold V] Title: 치킨 배달, Time: 252 ms, Memory: 18252 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -4,14 +4,13 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class Main {
-	static int N;						// 2차원 도시 배열의 행, 열 개수
-	static int M;						// 폐업시키지 않을 치킨집의 개수
-	static ArrayList<Point> houses;		// 집 좌표 리스트
-	static ArrayList<Point> chickens;	// 치킨집 좌표 리스트
-	static int[] open;					// 오픈한 치킨집 인덱스 배열
-	static boolean[] selected;			// 치킨집의 오픈 여부 배열
-	static int cityChickenLength;		// 도시의 치킨 거리의 최솟값	
+public class Main {			
+	static int M;											// 폐업시키지 않을 치킨집의 개수
+	static ArrayList<Point> houses = new ArrayList<>();		// 집 좌표 리스트
+	static ArrayList<Point> chickens = new ArrayList<>();	// 치킨집 좌표 리스트
+	static int[] open;										// 오픈한 치킨집 인덱스 배열
+	static boolean[] selected;								// 치킨집의 오픈 여부 배열
+	static int minCityChickenLength = Integer.MAX_VALUE;	// 도시의 치킨 거리의 최솟값	
 	
 	static class Point {
 		int x, y;
@@ -25,60 +24,72 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());	
+		int N = Integer.parseInt(st.nextToken());	// 2차원 도시 배열의 행, 열 개수	
 		M = Integer.parseInt(st.nextToken());	
-		houses = new ArrayList<>();
-		chickens = new ArrayList<>();
 		open = new int[M];
-		cityChickenLength = Integer.MAX_VALUE;
 		
 		// 2차원 도시 배열 초기화
 		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 1; j <= N; j++) {
 				int spot = Integer.parseInt(st.nextToken());
-				
 				if (spot == 1) {
-					houses.add(new Point(i, j));
+					houses.add(new Point(i, j));	// 집 좌표 리스트에 값 삽입
 				} else if (spot == 2) {
-					chickens.add(new Point(i, j));
+					chickens.add(new Point(i, j));	// 치킨집 좌표 리스트에 값 삽입
 				}
 			}
 		}
 		
-		selected = new boolean[chickens.size()];
+		// 치킨집 오픈 여부 배열의 사이즈를 치킨집의 개수만큼 정해주고 초기화
+		selected = new boolean[chickens.size()];	
 		
+		// M개의 치킨집을 선택한 경우의 수를 모두 탐색해야하므로 백트래킹 수행
 		dfs(0, 0);
 		
-		System.out.println(cityChickenLength);
+		System.out.println(minCityChickenLength);	// 도시의 치킨 거리의 최솟값 출력
 	}
 	
 	public static void dfs(int depth, int index) {
+		// M개의 치킨집을 모두 선택했으면 
+		// M개의 치킨집을 선택한 도시의 치킨 거리 계산 후 함수 종료
 		if (depth == M) {
-			int tempChickenLength = 0;
+			int cityChickenLength = 0;	// M개의 치킨집을 선택한 도시의 치킨 거리
 			
-			for (Point house : houses) {
-				int houseCheckenLength = Integer.MAX_VALUE;
+			for (Point house : houses) {	// 집들 모두 탐색
+				// 해당 집과 치킨집들 사이의 거리중 가장 작은 값
+				int chickenLength = Integer.MAX_VALUE;
 				
-				for (int i = 0; i < M; i++) {
+				for (int i = 0; i < M; i++) {	// 오픈한 치킨집 모두 탐색
+					// open[i]: i번째로 오픈한 치킨집의 인덱스
+					// chicken: open[i]번째 치킨집
 					Point chicken = chickens.get(open[i]);
-					int chickenlength = Math.abs(house.x - chicken.x) + Math.abs(house.y - chicken.y);
-					houseCheckenLength = Math.min(houseCheckenLength, chickenlength);
+					// 해당 집과 해당 치킨집 사이의 거리
+					int length = Math.abs(house.x - chicken.x) + Math.abs(house.y - chicken.y);
+					// 해당 집과 치킨집들 사이의 거리 최솟값 갱신
+					chickenLength = Math.min(chickenLength, length);
 				}
 				
-				tempChickenLength += houseCheckenLength;
+				// 해당 집의 최소 치킨 거리를 찾았으면 도시의 치킨 거리에 더하기
+				cityChickenLength += chickenLength;
 			}
 			
-			cityChickenLength = Math.min(cityChickenLength, tempChickenLength);
-			return;
+			// 도시의 치킨 거리의 최솟값 갱신
+			minCityChickenLength = Math.min(minCityChickenLength, cityChickenLength);
+			return;		// 함수 종료 후 다른 치킨집 오픈하러 가기
 		}
 		
+		// 백트래킹
+		// 0 ~ chicken.size() - 1번째 치킨집 모두 탐색
+		// 오픈된 치킨집은 총 M개이므로, depth번째 치킨집도 M번 들어가야 함(인덱스는 M - 1까지)
 		for (int i = index; i < chickens.size(); i++) {			
-			if (!selected[i]) {
-				selected[i] = true;
-				open[depth] = i;
-				dfs(depth + 1, i + 1);
-				selected[i] = false;
+			if (!selected[i]) {			// i번째 치킨집이 폐업 상태라면
+				selected[i] = true;		// i번째 치킨집 오픈시키기
+				open[depth] = i;		// depth번째 오픈된 치킨집 = i번째 치킨집
+				// depth + 1번째 치킨집 오픈하러 가기
+				// 앞에는 이미 탐색했으므로 i + 1번째부터 탐색해야 시간 복잡도가 줄어듦
+				dfs(depth + 1, i + 1);	
+				selected[i] = false;	// i번째 치킨집 폐업시키기
 			}
 		}
 	}
