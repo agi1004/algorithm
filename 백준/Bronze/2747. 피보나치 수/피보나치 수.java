6b51605bf4ce1f31723b00920f44e61d3b42b9a4
agi1004 <39743375+agi1004@users.noreply.github.com>
2024-06-24 20:09:57 +0900

    [Bronze II] Title: 피보나치 수, Time: 236 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] fibo = new int[n + 1];
+		
+		fibo[1] = 1;
+		
+		if (n >= 2) {
+			fibo[2] = 1;
+		}
+		
+		for (int i = 3; i <= n; i++) {
+			fibo[i] = fibo[i - 1] + fibo[i - 2];
+		}
+		
+		System.out.println(fibo[n]);
+		sc.close();
+	}
+}
