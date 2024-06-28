3cb8ecf31ef8e597ff4235fdc09dd880a70c2fe9
agi1004 <39743375+agi1004@users.noreply.github.com>
2024-06-29 06:48:17 +0900

    [Silver II] Title: A → B, Time: 172 ms, Memory: 24808 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.java
+++ 백준/Silver/16953. A → B/A → B.java
@@ -1,18 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
 import java.util.Set;
+import java.util.StringTokenizer;
 
 public class Main {
 	static int A, B;
 	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		A = sc.nextInt();
-		B = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		A = Integer.parseInt(st.nextToken());
+		B = Integer.parseInt(st.nextToken());
 		System.out.println(bfs());
-		sc.close();
 	}
 	
 	public static int bfs() {
