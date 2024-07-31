8a71e93b3413001c559ed3c20510c4f60926f7f6
agi1004 <39743375+agi1004@users.noreply.github.com>
2024-07-31 21:01:01 +0900

    [Silver II] Title: 생태학, Time: 1020 ms, Memory: 93100 KB -BaekjoonHub

--- 백준/Silver/4358. 생태학/생태학.java
+++ 백준/Silver/4358. 생태학/생태학.java
@@ -1,25 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Map;
-import java.util.Scanner;
 import java.util.TreeMap;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		Map<String, Integer> map = new TreeMap<>();
 		int totalCount = 0;
+		String s;
 		
-		while (sc.hasNext()) {
-			String name = sc.nextLine();
-			int count = (map.containsKey(name)) ? map.get(name) + 1 : 1;
-			map.put(name, count);
+		while ((s = br.readLine()) != null) {		
+			map.put(s, map.getOrDefault(s, 0) + 1);
 			totalCount++;
 		}
 		
 		for (String name : map.keySet()) {
-			double ratio = map.get(name) * 100.0 / totalCount;
-			System.out.printf("%s %.4f\n", name, ratio);
+			int count = map.get(name);
+			double ratio = count * 100.0 / totalCount;
+			bw.write(String.format("%s %.4f\n", name, ratio));
 		}
 		
-		sc.close();
+		bw.flush();
 	}
 }
