45fd520fa94986d7ba9e998a55c248ce9ccdf706
agi1004 <39743375+agi1004@users.noreply.github.com>
2024-09-26 12:08:36 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.09 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean solution(String s) {
+        int length = s.length();
+        
+        if (length != 4 && length != 6) {
+            return false;
+        }
+        
+        for (int i = 0; i < length; i++) {
+            if (!Character.isDigit(s.charAt(i))) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
