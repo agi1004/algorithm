8f117f168631b13e14892745871c82a1a300f0a4
agi1004 <39743375+agi1004@users.noreply.github.com>
2024-09-27 16:01:18 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.03 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int storey) {
+        int answer = 0;
+        
+        while (storey > 0) {
+            int digit = storey % 10;
+            storey /= 10;
+            
+            if (digit < 5) {
+                answer += digit;
+            } else if (digit > 5) {
+                answer += 10 - digit;
+                storey++;
+            } else {
+                answer += digit;
+                if (storey % 10 >= 5) {
+                    storey++;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
