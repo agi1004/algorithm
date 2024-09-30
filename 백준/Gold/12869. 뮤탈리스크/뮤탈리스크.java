9e8e2ce6a41f310a924cb85a6ef25a67bf4950fd
agi1004 <39743375+agi1004@users.noreply.github.com>
2024-09-30 17:31:16 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 112 ms, Memory: 16968 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.java
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.java
@@ -28,20 +28,25 @@ public class Main {
 		int s2 = scv[1];
 		int s3 = scv[2];
 		
+		// 공격 횟수 최솟값보다 현재 공격 횟수가 같거나 클 경우 중단
 		if (minCount <= count) {
 			return;
 		}
 		
+		// 이미 방문했는데 기존 공격 횟수가 더 작을 경우 중단
 		if (dp[s1][s2][s3] != 0 && dp[s1][s2][s3] <= count) {
 			return;
 		}
 		
 		dp[s1][s2][s3] = count;
 		
+		// 모든 scv가 죽을 경우 최솟값 갱신 및 중단
 		if (s1 == 0 && s2 == 0 && s3 == 0) {
 			minCount = Math.min(minCount, count);
+			return;
 		}
 		
+		// 6가지 공격 패턴으로 현재 scv를 공격한 후 넘김
 		for (int i = 0; i < 6; i++) {
 			dfs(new int[] {Math.max(s1 + attack[i][0], 0), 
 						   Math.max(s2 + attack[i][1], 0), 
