8f74af358c928de8e04b1011fe326abf9358cebe
agi1004 <39743375+agi1004@users.noreply.github.com>
2024-09-30 19:16:04 +0900

    [Gold IV] Title: 주난의 난(難), Time: 184 ms, Memory: 22896 KB -BaekjoonHub

--- 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.java
+++ 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.java
@@ -6,49 +6,41 @@ public class Main {
 	static int[] dy = {0, 0, -1, 1};
 	static int N, M;
 	static char[][] room;
-	static boolean[][] area;
-	static int count = 0;
+	static boolean[][] visited;
+	static List<int[]> list = new ArrayList<>();
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		room = new char[N + 1][M + 1];
+		room = new char[N][M];
+		visited = new boolean[N][M];
 		
 		st = new StringTokenizer(br.readLine());
-		int x1 = Integer.parseInt(st.nextToken());	// 주난 x좌표
-		int y1 = Integer.parseInt(st.nextToken());	// 주난 y좌표
-		int x2 = Integer.parseInt(st.nextToken());	// 범인 x좌표
-		int y2 = Integer.parseInt(st.nextToken());	// 범인 y좌표
+		int x1 = Integer.parseInt(st.nextToken()) - 1;
+		int y1 = Integer.parseInt(st.nextToken()) - 1;
 		
-		for (int i = 1; i <= N; i++) {
-			String line = br.readLine();
-			for (int j = 1; j <= M; j++) {
-				// 0: 빈 공간, 1: 친구, *: 주난, #: 범인
-				room[i][j] = line.charAt(j - 1);
-			}
+		for (int i = 0; i < N; i++) {
+			room[i] = br.readLine().toCharArray();
 		}
+		
+		list.add(new int[] {x1, y1});
+		visited[x1][y1] = true;
+		
+		int count = 0;
 		
 		while (true) {
 			count++;
-			
-			setJunanArea(x1, y1);
-			
-			if (findCriminal(x1, y1)) {
-				break;
-			}
+			if (bfs()) break;
 		}
 		
 		System.out.println(count);
 	}
 	
-	public static void setJunanArea(int startX, int startY) {
-		Queue<int[]> queue = new LinkedList<>();
-		area = new boolean[N + 1][M + 1];
-		
-		queue.add(new int[] {startX, startY});
-		area[startX][startY] = true;
+	public static boolean bfs() {
+		Queue<int[]> queue = new LinkedList<>(list);
+		list.clear();
 		
 		while (!queue.isEmpty()) {
 			int[] now = queue.poll();
@@ -58,36 +50,7 @@ public class Main {
 				int nx = x + dx[i];
 				int ny = y + dy[i];
 				
-				if (nx < 1 || ny < 1 || nx > N || ny > M) {
-					continue;
-				}
-				
-				if (area[nx][ny] || room[nx][ny] == '1') {
-					continue;
-				}
-				
-				queue.add(new int[] {nx, ny});
-				area[nx][ny] = true;
-			}
-		}
-	}
-	
-	public static boolean findCriminal(int startX, int startY) {
-		Queue<int[]> queue = new LinkedList<>();
-		boolean[][] visited = new boolean[N + 1][M + 1];
-		
-		queue.add(new int[] {startX, startY});
-		visited[startX][startY] = true;
-		
-		while (!queue.isEmpty()) {
-			int[] now = queue.poll();
-			int x = now[0], y = now[1];
-			
-			for (int i = 0; i < 4; i++) {
-				int nx = x + dx[i];
-				int ny = y + dy[i];
-				
-				if (nx < 1 || ny < 1 || nx > N || ny > M) {
+				if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
 					continue;
 				}
 				
@@ -95,13 +58,12 @@ public class Main {
 					continue;
 				}
 				
-				if (room[nx][ny] == '1') {
-					room[nx][ny] = '0';
-				} else if (room[nx][ny] == '#') {
+				if (room[nx][ny] == '#') {
 					return true;
-				}
-				
-				if (area[nx][ny]) {
+				} else if (room[nx][ny] == '1') {
+					list.add(new int[] {nx, ny});
+					visited[nx][ny] = true;
+				} else {
 					queue.add(new int[] {nx, ny});
 					visited[nx][ny] = true;
 				}
