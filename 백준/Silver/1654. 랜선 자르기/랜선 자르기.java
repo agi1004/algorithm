abec5ddaf166692168ca8457e768713815d0de8b
agi1004 <39743375+agi1004@users.noreply.github.com>
2024-10-01 22:45:10 +0900

    [Silver II] Title: 랜선 자르기, Time: 156 ms, Memory: 17628 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,54 +1,39 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int[] line;
-	//static long maxLength = 0;
-	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int K = Integer.parseInt(st.nextToken());	// 친구가 이미 가지고 있는 랜선의 개수
-		int N = Integer.parseInt(st.nextToken());	// 내가 필요한 랜선의 개수
-		line = new int[K];
+		int K = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		long[] lans = new long[K];
+		long maxLans = 0;
 		
 		for (int i = 0; i < K; i++) {
-			line[i] = Integer.parseInt(br.readLine());
+			lans[i] = Integer.parseInt(br.readLine());
+			maxLans = Math.max(maxLans, lans[i]);
 		}
 		
-		long start = 1;
-		long end = Arrays.stream(line).max().getAsInt();
-		long mid = (start + end) / 2;
-		long count = findCount(mid);
+		long start = 1, end = maxLans;
+		long maxLength = 0;
 		
 		while (start <= end) {
-			if (count == N) {
-//				maxLength = Math.max(maxLength, mid);
-				start = mid + 1;
-			} else if (count < N) {		// 더 작은 값으로 잘라야 함
-				end = mid - 1;
-			} else {					// 더 큰 값으로 잘라야 함
-				start = mid + 1;
+			long mid = (start + end) / 2;
+			long count = 0;
+			
+			for (long lan : lans) {
+				count += lan / mid;
 			}
 			
-			mid = (start + end) / 2;
-			count = findCount(mid);
+			if (count < N) {
+				end = mid - 1;
+			} else {
+				maxLength = Math.max(maxLength, mid);
+				start = mid + 1;
+			}
 		}
 		
-		//System.out.println(maxLength);
-		System.out.println(mid);
-	}
-	
-	public static long findCount(long length) {
-		long count = 0;
-		
-		for (int i = 0; i < line.length; i++) {
-			count += line[i] / length;
-		}
-		
-		return count;
+		System.out.println(maxLength);
 	}
 }
