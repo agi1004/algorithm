e204416f258b30b8a1a3e0d5c913d794eca266dc
agi1004 <39743375+agi1004@users.noreply.github.com>
2024-10-06 20:32:42 +0900

    [Gold IV] Title: 최단경로, Time: 656 ms, Memory: 109076 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,91 +1,74 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-// 가중치가 있는 그래프를 담기 위한 클래스 별도 구현하기 (다익스트라에 활용)
-class Edge implements Comparable<Edge> {
-	int v, w;
-	
-	Edge(int v, int w) {
-		this.v = v;
-		this.w = w;
-	}
-	
-	// 가중치가 작은 것이 우선순위 큐에 먼저 들어가게 하도록 설정
-	public int compareTo(Edge e) {
-		if (this.w > e.w)
-			return 1;
-		else
-			return -1;
-	}
-}
-
-public class Main { 	
+public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
 		
-		int V = Integer.parseInt(st.nextToken());	// 정점의 개수
-		int E = Integer.parseInt(st.nextToken());	// 간선의 개수
-		int K = Integer.parseInt(br.readLine());	// 시작 정점의 번호
-		ArrayList<Edge>[] graph = new ArrayList[V + 1];
+		int V = Integer.parseInt(st.nextToken());
+		int E = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(br.readLine());
+		
+		List<Node>[] graph = new ArrayList[V + 1];
 		boolean[] visited = new boolean[V + 1];
-		int[] distance = new int[V + 1];	// 최단 거리 배열
-		PriorityQueue<Edge> pq = new PriorityQueue<Edge>();	// 다익스트라에 활용할 우선순위 큐
+		int[] distance = new int[V + 1];
+		Queue<Node> pq = new PriorityQueue<>((o1, o2) -> o1.w - o2.w);
 		
 		for (int i = 1; i <= V; i++) {
-			graph[i] = new ArrayList<Edge>();
+			graph[i] = new ArrayList<>();
 		}
 		
-		for (int i = 0; i <= V; i++) {
+		for (int i = 1; i <= V; i++) {
 			distance[i] = Integer.MAX_VALUE;
 		}
 		
-		// 가중치가 있는 인접 리스트 초기화하기
 		for (int i = 0; i < E; i++) {
 			st = new StringTokenizer(br.readLine());
 			int u = Integer.parseInt(st.nextToken());
 			int v = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());	// 가중치
-			graph[u].add(new Edge(v, w));
+			int w = Integer.parseInt(st.nextToken());
+			graph[u].add(new Node(v, w));
 		}
 		
-		// 출발 노드는 우선순위 큐에 넣고 시작하기
-		// 자동으로 거리가 최소인 노드를 선택하게 함
-		pq.add(new Edge(K, 0));	// K를 시작점으로 설정하기
 		distance[K] = 0;
+		pq.add(new Node(K, 0));
 		
 		while (!pq.isEmpty()) {
-			Edge now = pq.poll();
-			int now_v = now.v;
+			Node now = pq.poll();
 			
-			// 이미 방문한 적이 있는 노드는 다시 큐에 넣지 않음
-			if (visited[now_v]) 
+			if (visited[now.v]) {
 				continue;
-			visited[now_v] = true;
+			}
 			
-			for (int i = 0; i < graph[now_v].size(); i++) {
-				Edge next = graph[now_v].get(i);
-				int next_v = next.v;
-				int next_w = next.w;
-				
-				// 최소 거리로 업데이트하기
-				if (distance[next_v] > distance[now_v] + next_w) {
-					distance[next_v] = distance[now_v] + next_w;
-					pq.add(new Edge(next_v, distance[next_v]));
+			visited[now.v] = true;
+			
+			for (Node next : graph[now.v]) {
+				if (distance[next.v] > distance[now.v] + next.w) {
+					distance[next.v] = distance[now.v] + next.w;
+					pq.add(new Node(next.v, distance[next.v]));
 				}
 			}
 		}
 		
-		// 거리 배열 출력하기
 		for (int i = 1; i <= V; i++) {
-			if (visited[i])
-				System.out.println(distance[i]);
-			else
-				System.out.println("INF");
+			if (visited[i]) {
+				sb.append(distance[i] + "\n");
+			} else {
+				sb.append("INF\n");
+			}
+		}
+		
+		System.out.println(sb);
+	}
+	
+	static class Node {
+		int v, w;
+		
+		Node(int v, int w) {
+			this.v = v;
+			this.w = w;
 		}
 	}
 }
