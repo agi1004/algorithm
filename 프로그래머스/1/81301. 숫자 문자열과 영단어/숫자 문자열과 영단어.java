ade238f734990c795dc79709e5aca94af19f22fe
agi1004 <39743375+agi1004@users.noreply.github.com>
2024-11-15 14:18:14 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.10 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(String s) {
+        return Integer.parseInt(convertToNumber(s));
+    }
+    
+    private String convertToNumber(String s) {
+        return s.replace("zero", "0")
+                .replace("one", "1")
+                .replace("two", "2")
+                .replace("three", "3")
+                .replace("four", "4")
+                .replace("five", "5")
+                .replace("six", "6")
+                .replace("seven", "7")
+                .replace("eight", "8")
+                .replace("nine", "9");
+    }
+}
